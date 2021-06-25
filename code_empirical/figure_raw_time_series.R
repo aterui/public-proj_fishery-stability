758e6d6838adb45bc7504ad9558969aed06bf088
AkiraTerui <hanabi0111@gmail.com>
2021-06-24 22:15:24 -0400

    add: figure

---
+++ code_empirical/figure_raw_time_series.R
@@ -0,0 +1,28 @@
+
+# setup -------------------------------------------------------------------
+  
+  # remove site 4 at usubetsu - no coordinates data
+  
+  rm(list = ls())
+  pacman::p_load(tidyverse, foreach)
+  setwd(here::here("code_empirical"))
+  
+
+# read data ---------------------------------------------------------------
+  
+  source("data_fmt_fishdata.R")
+  
+# plot --------------------------------------------------------------------
+
+  d0 %>% 
+    ggplot() +
+    geom_point(aes(x = year,
+                   y = density,
+                   color = factor(site))) + 
+    geom_line(aes(x = year,
+                  y = density,
+                  color = factor(site))) + 
+    facet_wrap(facets = ~ river,
+               ncol = 5) +
+    theme_bw()
+  
