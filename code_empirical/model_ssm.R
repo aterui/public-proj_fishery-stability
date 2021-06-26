24190866ce6bd10ce720c035002c16a1210c3367
aterui <hanabi0111@gmail.com>
2021-06-26 15:10:52 -0400

    edits: model files

--- code_empirical/model_ssm.R
+++ code_empirical/model_ssm.R
@@ -1,52 +1,52 @@
 model {
   
-  ninfo <- 0.01
+  ninfo <- 0.1
 
 # prior -------------------------------------------------------------------
   
   for (j in 1:Nsite) {
-    tau_r_time[j] ~ dscaled.gamma(10, 1)
+    tau_r_time[j] ~ dscaled.gamma(2.5, 1)
     sd_r_time[j] <- sqrt(1 / tau_r_time[j])
     
-    tau_obs[j] ~ dscaled.gamma(10, 1)
+    tau_obs[j] ~ dscaled.gamma(2.5, 1)
     sd_obs[j] <- sqrt(1 / tau_obs[j])
     
     log_mu_r[j] ~ dnorm(log_global_r, tau_r_space)
   }
   
   log_global_r ~ dnorm(0, ninfo)
-  tau_r_space ~ dscaled.gamma(10, 1)
+  tau_r_space ~ dscaled.gamma(2.5, 1)
   sd_r_space <- sqrt(1 / tau_r_space)
   
   for (j in 1:Nsite) {
-    log_d[St_year[j], j] ~ dnorm(0, ninfo)
+    log_d[j, St_year[j]] ~ dnorm(0, ninfo)
   }
   
 # likelihood --------------------------------------------------------------
   
   for (i in 1:Nsample) {
-    N[i] ~ dpois(d_obs[Year[i], Site[i]] * Area[i])
+    N[i] ~ dpois(n_obs[Site[i], Year[i]])
+    log(n_obs[Site[i], Year[i]]) <- log_d_obs[Site[i], Year[i]] + log(Area[i])
   }
   
   for (j in 1:Nsite) {
     for (t in St_year[j]:End_year[j]) {
-      log(d_obs[t, j]) <- log_d_obs[t, j]
-      log_d_obs[t, j] ~ dnorm(log_d[t, j], tau_obs[j])
-      log(d[t, j]) <- log_d[t, j]
+      log_d_obs[j, t] ~ dnorm(log_d[j, t], tau_obs[j])
+      log(d[j, t]) <- log_d[j, t]
     }
   }  
   
   for (j in 1:Nsite) {
     for (t in St_year[j]:(End_year[j] - 1)) {
-      log_d[t + 1, j] <- log_r[t, j] + log_d[t, j]
-      log_r[t, j] ~ dnorm(log_mu_r[j], tau_r_time[j])
+      log_d[j, t + 1] <- log_r[j, t] + log_d[j, t]
+      log_r[j, t] ~ dnorm(log_mu_r[j], tau_r_time[j])
     }
   }
   
 # statistical quantity ----------------------------------------------------
 
   for (j in 1:Nsite) {
-    cv[j] <- sd(d[St_year[j]:End_year[j], j]) / mean(d[St_year[j]:End_year[j], j])
+    cv[j] <- sd(d[j, St_year[j]:End_year[j]]) / mean(d[j, St_year[j]:End_year[j]])
   }
     
 }
--- code_empirical/run_model_ssm.R
+++ code_empirical/run_model_ssm.R
@@ -21,6 +21,10 @@
                  
   para <- c("log_global_r",
             "sd_r_space",
+            "log_mu_r",
+            "sd_r_time",
+            "sd_obs",
+            "log_d",
             "cv")
   
   m <- read.jagsfile("model_ssm.R")
@@ -30,16 +34,16 @@
 
   n_ad <- 100
   n_iter <- 1.0E+4
-  n_thin <- 50
+  n_thin <- max(3, ceiling(n_iter / 500))
   n_burn <- ceiling(max(10, n_iter/2))
-  n_sample <- ceiling(n_iter/n_thin)
+  n_sample <- ceiling(n_iter / n_thin)
   
   inits <- replicate(3,
                      list(.RNG.name = "base::Mersenne-Twister",
                           .RNG.seed = NA),
                      simplify = FALSE)
   
-  for (i in 1:3) inits[[i]]$.RNG.seed <- i + 100
+  for (i in 1:3) inits[[i]]$.RNG.seed <- i + 1
 
     
 # run jags ----------------------------------------------------------------
@@ -58,3 +62,63 @@
                    module = "glm")
   
   mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
+  max(mcmc_summary$Rhat)
+  
+  while(max(mcmc_summary$Rhat) > 1.09) {
+    post <- extend.jags(post,
+                        burnin = 0,
+                        sample = n_sample,
+                        adapt = n_ad,
+                        thin = n_thin,
+                        n.sims = 3,
+                        combine = TRUE)
+    
+    mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
+    print(max(mcmc_summary$Rhat))
+  }
+  
+  n_total_mcmc <- (post$sample / n_sample) * n_iter + n_burn
+  
+  
+# format output -----------------------------------------------------------
+  
+  fn_brrm <- function(x) {
+    y <- lapply(str_extract_all(x, pattern = "\\[.{1,}\\]"),
+                FUN = function(z) ifelse(identical(z, character(0)),
+                                         NA,
+                                         z))
+    str_remove_all(y, pattern = "\\[|\\]")
+  }
+  
+  param <- rownames(mcmc_summary)
+  param_name <- str_remove(param,
+                           pattern = "\\[.{1,}\\]")
+  
+  dat_site <- dat %>% 
+    group_by(site_id_numeric) %>% 
+    summarize(river = unique(river),
+              site = unique(site),
+              site_id = unique(site_id))
+  
+  est <- mcmc_summary %>% 
+    as_tibble() %>% 
+    mutate(n_total_mcmc = n_total_mcmc,
+           n_sample = post$sample,
+           n_thin = n_thin,
+           n_burn = n_burn,
+           param_name = param_name,
+           param = param,
+           character_id = fn_brrm(param)) %>% 
+    separate(character_id, into = c("site_id", "year_id")) %>% 
+    mutate(site_id_numeric = suppressWarnings(as.numeric(site_id)),
+           year_id = as.numeric(year_id),
+           year = year_id + 1998) %>% 
+    select(-site_id) %>% 
+    left_join(dat_site, by = "site_id_numeric") %>% 
+    left_join(dat, by = c("year",
+                          "river",
+                          "site",
+                          "site_id",
+                          "site_id_numeric"))
+    
+  write_csv(est, "data_fmt/data_ssm_est.csv")
