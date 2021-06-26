641cd6b34f584eecc251d7517480b0dd5024f898
aterui <hanabi0111@gmail.com>
2021-06-26 15:11:10 -0400

    edits: figure

--- code_empirical/figure_time_series.R
+++ code_empirical/figure_time_series.R
@@ -1,7 +1,6 @@
 
 # setup -------------------------------------------------------------------
 
-  rm(list = ls())
   pacman::p_load(tidyverse, broom)
   setwd(here::here("code_empirical"))
 
@@ -9,20 +8,27 @@
 # read data ---------------------------------------------------------------
 
   d0 <- read_csv("data_fmt/data_ssm_est.csv") %>% 
-    filter(param_id == "log_d_mean") %>%
+    filter(param_name == "log_d") %>%
     rename(median = "50%",
            lower = "2.5%",
            upper = "97.5%") %>% 
-    mutate(abundance = exp(median)*100)
+    mutate(est_density = exp(median))
+           
   
 
 # plot --------------------------------------------------------------------
 
   d0 %>% 
     ggplot() + 
-    geom_line(aes(x = year_id, y = abundance, color = species)) +
-    geom_point(aes(x = year_id, y = abundance, color = species),
-               alpha = 0.3) +
-    facet_wrap(facets = ~ river, ncol = 6, scales = "free_y") +
+    geom_line(aes(x = year_id,
+                  y = est_density,
+                  color = factor(site))) +
+    geom_point(aes(x = year_id,
+                   y = density,
+                   color = factor(site)),
+               alpha = 0.5) +
+    facet_wrap(facets = ~ river,
+               ncol = 6,
+               scales = "free_y") +
     theme_bw()
   
