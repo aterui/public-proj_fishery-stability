80597f3a07bc84a649d10e92f769fe04f174df82
AkiraTerui <hanabi0111@gmail.com>
2021-08-04 23:15:35 -0400

    Update analysis_mu_species-level.R

--- code_empirical/analysis_mu_species-level.R
+++ code_empirical/analysis_mu_species-level.R
@@ -4,8 +4,7 @@
 rm(list = ls())
 pacman::p_load(tidyverse,
                foreach,
-               lme4,
-               brms)
+               glmmTMB)
 setwd(here::here("code_empirical"))
 
 
@@ -14,22 +13,28 @@ setwd(here::here("code_empirical"))
 source("data_fmt_fishdata.R")
 
 df_env <- read_csv("data_fmt/data_env_fmt.csv") %>% 
-  rename(wsd_area = area)
+  rename(wsd_area = area) %>% 
+  mutate(scl_wsd_area = c(scale(wsd_area)),
+         scl_ppt = c(scale(ppt)),
+         scl_temp = c(scale(temp)),
+         scl_forest = c(scale(frac_forest)))
 
-df_ocean <- read_csv("data_fmt/data_ocean_fmt.csv")
+df_ocean <- read_csv("data_fmt/data_ocean_fmt.csv") %>% 
+  mutate(scl_chr_a = c(scale(chr_a)),
+         scl_sst = c(scale(sst)))
 
 df_stock <- read_csv("data_fmt/data_hkd_prtwsd_stock_fmt.csv") %>% 
   filter(between(year_release, 1999, 2019)) %>% 
   group_by(river) %>% 
-  summarize(mean_stock = sum(abundance) / (2019 - 1999 + 1),
-            sd_stock = sqrt(sum((abundance - mean_stock)^2) / (2019 - 1999)))
+  summarize(mean_stock = sum(abundance) / (2019 - 1999 + 1)) %>% 
+  mutate(scl_mean_stock = c(scale(mean_stock)))
 
 df_m <- d0 %>% 
   left_join(df_env, by = c("river", "site")) %>% 
   left_join(df_stock, by = "river") %>% 
   left_join(df_ocean, by = "river") %>% 
   mutate(mean_stock = ifelse(is.na(mean_stock), 0, mean_stock),
-         sd_stock = ifelse(is.na(sd_stock), 0, sd_stock))
+         scl_mean_stock = ifelse(is.na(scl_mean_stock), 0, scl_mean_stock))
 
 
 # data selection ----------------------------------------------------------
@@ -47,69 +52,56 @@ df_m <- df_m %>%
   filter(taxon %in% selected_taxa) %>% 
   mutate(site = factor(site))
 
-## watershed selection ####
-df_subset <- foreach(i = seq_len(n_distinct(df_m$taxon)),
-                     .combine = bind_rows) %do% {
-                       
-                       river_subset <- df_m %>% 
-                         filter(taxon == selected_taxa[i]) %>% 
-                         group_by(river) %>% 
-                         summarize(n = sum(abundance)) %>% 
-                         filter(n > 0) %>% 
-                         pull(river)
-                       
-                       df0 <- df_m %>% 
-                         filter(river %in% river_subset & taxon == selected_taxa[i])
-                       
-                       return(df0)  
-                     }
-
-df_subset <- df_subset %>% 
-  mutate(density = abundance / area)
-
 ## taking average for watersheds ####
-df_river <- df_subset %>% 
+df_river <- df_m %>% 
   group_by(river, taxon) %>% 
   summarize(mean_density = mean(abundance / area),
             mean_stock = unique(mean_stock))
+
+## taking average for site ####
+df_site <- df_m %>% 
+  group_by(site_id, taxon) %>% 
+  summarize(mean_density = mean(abundance / area),
+            mean_stock = unique(mean_stock))
+
+## watershed selection ####
+#f_subset <- foreach(i = seq_len(n_distinct(df_m$taxon)),
+#                     .combine = bind_rows) %do% {
+#                       
+#                       river_subset <- df_m %>% 
+#                         filter(taxon == selected_taxa[i]) %>% 
+#                         group_by(river) %>% 
+#                         summarize(n = sum(abundance)) %>% 
+#                         filter(n > 0) %>% 
+#                         pull(river)
+#                       
+#                       df0 <- df_m %>% 
+#                         filter(river %in% river_subset & taxon == selected_taxa[i])
+#                       
+#                       return(df0)  
+#                     }
+
+#df_subset <- df_subset %>% 
+#  mutate(density = abundance / area)
 
 
 # analysis ----------------------------------------------------------------
 
 list_m <- foreach(i = seq_len(n_distinct(df_m$taxon))) %do% {
   
-  #re <- df_subset %>% 
-  #  filter(taxon == selected_taxa[i]) %>% 
-  #  glmer(abundance ~ scale(mean_stock) +
-  #                    scale(chr_a) +
-  #                    scale(wsd_area) + 
-  #                    scale(temp) + 
-  #                    scale(frac_forest) + 
-  #                    scale(ppt) +
-  #                    offset(log(area)) + 
-  #                    (1 | river) +
-  #                    (1 | site:river),
-  #        family = "poisson",
-  #        data = .)
-  
-  re <- df_subset %>% 
+  re <- df_m %>% 
     filter(taxon == selected_taxa[i]) %>% 
-    brm(abundance ~ scale(mean_stock) +
-                    scale(chr_a) +
-                    scale(wsd_area) + 
-                    scale(temp) + 
-                    scale(frac_forest) + 
-                    scale(ppt) +
-                    offset(log(area)) + 
-                    (1 | river) +
-                    (1 | site:river),
-        family = negbinomial(),
-        prior = c(prior(normal(0, 10), class = b),
-                  prior(student_t(3, 0, 10), class = Intercept),
-                  prior(student_t(3, 0, 10), class = sd)),
-        data = .,
-        cores = 8,
-        adapt_delta = 1.5)
+    glmmTMB(abundance ~ scl_mean_stock +
+                        scl_chr_a +
+                        scl_wsd_area + 
+                        scl_temp + 
+                        scl_forest + 
+                        scl_ppt +
+                        offset(log(area)) + 
+                        (1 | river) +
+                        (1 | site:river),
+            family = "nbinom2",
+            data = .)
   
   return(re)  
 }
@@ -123,10 +115,11 @@ df_river %>%
   ggplot() +
   geom_point(aes(y = mean_density,
                  x = mean_stock)) +
-  geom_point(aes(y = density,
+  geom_point(aes(y = mean_density,
                  x = mean_stock),
              alpha = 0.1,
-             data = df_subset) +
+             size = 1,
+             data = df_site) +
   facet_wrap(facets = ~taxon,
              scales = "free",
              ncol = 4) +
