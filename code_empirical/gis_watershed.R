ca9574fe5c4b6f16ee8d94649712dae4c09efffe
AkiraTerui <hanabi0111@gmail.com>
2021-09-15 23:33:28 -0400

    edits: introduce whitebox tools

--- code_empirical/gis_watershed.R
+++ code_empirical/gis_watershed.R
@@ -6,12 +6,18 @@ rm(list = ls())
 pacman::p_load(tidyverse,
                foreach,
                sf,
+               stars,
                raster,
-               RSAGA,
-               maptools)
+               whitebox,
+               doParallel, 
+               doSNOW)
+
+wbt_init()
 
 setwd(here::here("code_empirical"))
 source("gis_crs_fmt.R")
+source("function_arc2d8.R")
+
 
 # define function ---------------------------------------------------------
 
@@ -33,93 +39,112 @@ st_snap_points = function(x, y, max_dist = 1000) {
 }
 
 
+# snap to the nearest streamline -------------------------------------------
 
-# snap to the nearest stremline -------------------------------------------
-
-albers_sf_channel <- st_read(dsn = "data_gis",
-                             layer = "epsg4326_channel_1sqkm") %>% 
-  st_transform(crs = wkt_jgd_albers)
-
-albers_sf_site <- read_csv("data_gis/site-coordinate_hogosuimen_terui-org-2019.csv") %>% 
-  drop_na(longitude) %>% 
-  st_as_sf(coords = c("longitude", "latitude")) %>% 
-  st_set_crs(4326) %>% 
-  st_transform(crs = st_crs(albers_sf_channel))
-
-albers_sf_site_snap <- st_snap_points(albers_sf_site, albers_sf_channel)
-df_site_snap_coord <- st_coordinates(albers_sf_site_snap) %>% 
-  as_tibble() %>% 
-  bind_cols(albers_sf_site) %>% 
-  dplyr::select(-geometry)
-
-albers_sf_site_snap_coord <- st_as_sf(df_site_snap_coord,
-                                      coords = c("X", "Y")) %>% 
-  st_set_crs(st_crs(albers_sf_channel))
-
-st_write(albers_sf_site_snap_coord,
-         "data_gis/albers_point_snap_prtwsd.gpkg",
-         append = FALSE)
+#albers_sf_channel <- st_read(dsn = "data_gis/epsg4326_channel_1sqkm.gpkg") %>% 
+#  st_transform(crs = wkt_jgd_albers)
+#
+#albers_sf_site <- read_csv("data_gis/site-coordinate_hogosuimen_terui-org-2019.csv") %>% 
+#  drop_na(longitude) %>% 
+#  st_as_sf(coords = c("longitude", "latitude")) %>% 
+#  st_set_crs(4326) %>% 
+#  st_transform(crs = st_crs(albers_sf_channel))
+#
+#albers_sf_site_snap <- st_snap_points(albers_sf_site, albers_sf_channel)
+#df_site_snap_coord <- st_coordinates(albers_sf_site_snap) %>% 
+#  as_tibble() %>% 
+#  bind_cols(albers_sf_site) %>% 
+#  dplyr::select(-geometry)
+#
+#wgs84_sf_site_snap_coord <- st_as_sf(df_site_snap_coord,
+#                                     coords = c("X", "Y")) %>% 
+#  st_set_crs(st_crs(albers_sf_channel)) %>% 
+#  st_transform(crs = 4326)
+#
+#st_write(wgs84_sf_site_snap_coord,
+#         dsn = "data_gis/epsg4326_point_snap_prtwsd.gpkg",
+#         append = FALSE)
 
 
-# watershed delineation ---------------------------------------------------
+# watershed delineation with whitebox -------------------------------------
 
-#rsaga.get.libraries()
-#rsaga.get.modules("ta_hydrology")
-#rsaga.get.usage("ta_hydrology", module = 4)
+# whitebox functions cannot take path including whitespace
+# gis files for whitebox is saved in temporary directory
+# NOTE: "epsg4326_point_snap_prtwsd_edit.gpkg" was manually edited to adjust around confluences
+# source file "epsg4326_point_snap_prtwsd.gpkg"
 
-#"albers_point_outlet_prtwsd":
-#"albers_point_snap_prtwsd.gpkg" was manually edited to delineate correct watershed polygons
-albers_sf_outlet <- st_read("data_gis/albers_point_outlet_prtwsd.gpkg")
+## flow direction
+wgs84_dir_arc <- raster::raster("data_gis/epsg4326_dir.tif")
+wgs84_dir_d8 <- arc2d8(wgs84_dir_arc)
 
-df_outlet_coord <- st_coordinates(albers_sf_outlet) %>% 
-  as_tibble() %>% 
-  bind_cols(albers_sf_outlet) %>% 
-  dplyr::select(-geom)
+temp_d8 <- paste(tempdir(), "epsg4326_dir_d8.tif", sep = "\\")
+
+raster::writeRaster(wgs84_dir_d8,
+                    temp_d8,
+                    overwrite = TRUE)
+
+## pour point
+wgs84_outlet <- sf::st_read("data_gis/epsg4326_point_snap_prtwsd_edit.gpkg")
+
+## parallel setup
+cl <- makeCluster(detectCores())
+registerDoSNOW(cl)
+
+pb <- txtProgressBar(max = nrow(wgs84_outlet), style = 3)
+fun_progress <- function(n) setTxtProgressBar(pb, n)
+opts <- list(progress = fun_progress)
+
+## run wbt_watershed 
+wgs84_watershed <- NULL
+wgs84_watershed <- foreach(i = seq_len(nrow(wgs84_outlet)),
+                           .combine = bind_rows,
+                           .packages = c("sf",
+                                         "raster",
+                                         "dplyr",
+                                         "whitebox",
+                                         "stars")
+                           ) %dopar% {
+                             
+  st_write(wgs84_outlet[i,],
+           dsn = tempdir(),
+           "outlet",
+           driver = "ESRI Shapefile",
+           append = FALSE,
+           quiet = TRUE)
   
-
-albers_sf_wsd <- foreach(i = seq_len(nrow(df_outlet_coord)),
-                         .combine = dplyr::bind_rows) %do% {
+  ### watershed raster
+  wsd <- paste(tempdir(), "epsg4326_watershed.tif", sep = "\\")
   
-  rsaga.geoprocessor(lib = "ta_hydrology",
-                     module = 4,
-                     param = list(TARGET_PT_X = df_outlet_coord$X[i], 
-                                  TARGET_PT_Y = df_outlet_coord$Y[i], 
-                                  ELEVATION = "tempdir/albers_filled_dem_hkd.sgrd", 
-                                  AREA = "tempdir/raster.sgrd",
-                                  METHOD = 0))
-                           
-  wsd <- stars::read_stars("tempdir/raster.sdat")
-  st_crs(wsd) <- st_crs(albers_sf_channel)
-  wsd[wsd == 0] <- NA
-  wsd_polygon <- st_as_sf(wsd,
-                          as_points = FALSE,
-                          merge = TRUE)
+  wbt_watershed(d8_pntr = temp_d8,
+                pour_pts = paste(tempdir(),
+                                 "outlet.shp",
+                                 sep = "\\"), 
+                output = wsd)
   
-  wsd_polygon <- wsd_polygon %>% 
-    mutate(id = i,
-           river = df_outlet_coord$river[i],
-           site = df_outlet_coord$site[i])
+  ### raster to polygon
+  wgs84_wsd_shape <- NULL
+  wgs84_wsd_shape <- raster::raster(wsd) %>% 
+    st_as_stars() %>% 
+    st_as_sf(merge = TRUE,
+             as_points = FALSE) %>% 
+    mutate(area = st_area(.)) %>% 
+    slice(which.max(area))
   
-  file.remove(c("tempdir/raster.mgrd",
-                "tempdir/raster.prj",
-                "tempdir/raster.sdat",
-                "tempdir/raster.sgrd"))
-  
-  return(wsd_polygon)
+  return(wgs84_wsd_shape)
 }
 
-st_write(albers_sf_wsd,
-         dsn = "data_gis/albers_wsd_prtwsd.gpkg",
+stopCluster(cl)
+
+mapview::mapview(wgs84_watershed) + mapview::mapview(wgs84_outlet)
+
+
+# export ------------------------------------------------------------------
+
+## attach site information
+wgs84_watershed <- wgs84_watershed %>% 
+  mutate(river = wgs84_outlet$river,
+         site = wgs84_outlet$site)
+
+st_write(wgs84_watershed,
+         dsn = "data_gis/epsg4326_watershed.gpkg",
          append = FALSE)
-
-# mapping -----------------------------------------------------------------
-
-albers_sf_site_snap <- st_read("data_gis/albers_point_snap_prtwsd.gpkg")
-albers_sf_wsd <- st_read("data_gis/albers_wsd_prtwsd.gpkg")
-
-mapview::mapView(albers_sf_site_snap) +
-  mapview::mapView(albers_sf_outlet,
-                   color = "red") +
-  mapview::mapView(albers_sf_wsd)
-  
-
