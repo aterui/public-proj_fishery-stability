02451cde86756c1cc89b8624c13e538b70597f7c
aterui <hanabi0111@gmail.com>
2021-10-13 12:14:07 -0400

    edits: data format
    correct errors in scaling
    simplify stock data formatting

--- code_empirical/data_fmt_analysis.R
+++ code_empirical/data_fmt_analysis.R
@@ -9,32 +9,39 @@ setwd(here::here("code_empirical"))
 
 source("data_fmt_fishdata.R")
 
+## df for species richness
 df_sp <- d0 %>% 
   filter(abundance > 0) %>% 
   group_by(river, site, site_id) %>% 
   summarize(n_species = n_distinct(taxon),
             n_species_unstock = n_distinct(taxon[.data$taxon != "Oncorhynchus_masou_masou"])) %>% 
-  ungroup() %>% 
-  mutate(scl_n_species = c(scale(n_species)),
-         scl_n_species_unstock = c(scale(n_species_unstock)))
+  ungroup()
 
+## df for environmental covariates
 df_env <- read_csv("data_fmt/data_env_fmt.csv") %>% 
-  rename(wsd_area = area) %>% 
-  mutate(scl_wsd_area = c(scale(wsd_area)),
-         scl_ppt = c(scale(ppt)),
-         scl_temp = c(scale(temp)),
-         scl_forest = c(scale(frac_forest)))
+  rename(wsd_area = area)
 
-df_ocean <- read_csv("data_fmt/data_ocean_fmt.csv") %>% 
-  mutate(scl_chr_a = c(scale(chr_a)),
-         scl_sst = c(scale(sst)))
+## df for ocean environments
+df_ocean <- read_csv("data_fmt/data_ocean_fmt.csv")
 
+## df for stock data
 df_stock <- read_csv("data_fmt/data_hkd_prtwsd_stock_fmt.csv") %>% 
   filter(between(year_release, 1999, 2019)) %>% 
+  group_by(year_release, river) %>% 
+  summarize(abundance = sum(abundance),
+            unit = unique(abundance_unit))
+
+df_ys <- tibble(year_release = rep(1999:2019,
+                                   nrow(distinct(df_env, river))),
+                river = rep(pull(distinct(df_env, river)),
+                            each = length(1999:2019)))
+
+df_stock_mu <- df_stock %>%
+  right_join(df_ys, by = c("year_release", "river")) %>% 
+  mutate(abundance = replace_na(abundance, 0),
+         unit = replace_na(unit, "thousand_fish")) %>% 
   group_by(river) %>% 
-  summarize(mean_stock = sum(abundance) / (2019 - 1999 + 1)) %>% 
-  ungroup() %>% 
-  mutate(scl_mean_stock = c(scale(mean_stock)))
+  summarize(mean_stock = mean(abundance))
 
 
 # ssm data ----------------------------------------------------------------
@@ -63,10 +70,8 @@ list_ssm <- foreach(i = seq_len(length(file_name))) %do% {
   df_m <- df_ssm %>% 
     left_join(df_env, by = c("river", "site")) %>% 
     left_join(df_sp, by = c("river", "site", "site_id")) %>% 
-    left_join(df_stock, by = "river") %>% 
-    left_join(df_ocean, by = "river") %>% 
-    mutate(mean_stock = replace_na(mean_stock, 0),
-           scl_mean_stock = replace_na(scl_mean_stock, 0))
+    left_join(df_stock_mu, by = "river") %>% 
+    left_join(df_ocean, by = "river")
   
   return(df_m)
 }
