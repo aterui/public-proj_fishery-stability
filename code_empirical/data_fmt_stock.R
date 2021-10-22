b6313aa8b7b8eeed4c9aee0376d453ea1a9d48a4
aterui <hanabi0111@gmail.com>
2021-10-22 15:55:30 -0400

    Update data_fmt_stock.R

--- code_empirical/data_fmt_stock.R
+++ code_empirical/data_fmt_stock.R
@@ -19,14 +19,14 @@ river_id <- pull(distinct(df_fish, river))
 df_site_id <- df_fish %>% 
   distinct(river, site_id, site_id_numeric) %>%
   left_join(tibble(year_release = rep(1999:2019,
-                                      nrow(distinct(df_fish, river))),
-                   river = rep(pull(distinct(df_fish, river)),
+                                      length(river_id)),
+                   river = rep(river_id,
                                each = length(1999:2019))),
             by = "river")
 
 df_year_river <- tibble(year_release = rep(1999:2019,
-                                           nrow(distinct(df_fish, river))),
-                        river = rep(pull(distinct(df_fish, river)),
+                                           length(river_id)),
+                        river = rep(river_id,
                                     each = length(1999:2019)))
 
 df_stock <- read_csv("data_fmt/data_hkd_prtwsd_stock_fmt.csv") %>% 
