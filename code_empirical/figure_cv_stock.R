a7fc901ff779fd1413033e6da34f090935a3916b
aterui <hanabi0111@gmail.com>
2021-10-24 13:39:39 -0400

    Update figure_cv_stock.R
    simplify

--- code_empirical/figure_cv_stock.R
+++ code_empirical/figure_cv_stock.R
@@ -8,85 +8,28 @@ setwd(here::here("code_empirical"))
 
 # data --------------------------------------------------------------------
 
-source("data_fmt_fishdata.R")
-
-df_sp <- d0 %>% 
-  filter(abundance > 0) %>% 
-  group_by(river, site, site_id) %>% 
-  summarize(n_species = n_distinct(taxon)) %>% 
-  ungroup() %>% 
-  mutate(scl_n_species = c(scale(n_species)))
-
-df_env <- read_csv("data_fmt/data_env_fmt.csv") %>% 
-  rename(wsd_area = area) %>% 
-  mutate(scl_wsd_area = c(scale(wsd_area)),
-         scl_ppt = c(scale(ppt)),
-         scl_temp = c(scale(temp)),
-         scl_forest = c(scale(frac_forest)))
-
-df_ocean <- read_csv("data_fmt/data_ocean_fmt.csv") %>% 
-  mutate(scl_chr_a = c(scale(chr_a)),
-         scl_sst = c(scale(sst)))
-
-df_stock <- read_csv("data_fmt/data_hkd_prtwsd_stock_fmt.csv") %>% 
-  filter(between(year_release, 1999, 2019)) %>% 
-  group_by(river) %>% 
-  summarize(mean_stock = sum(abundance) / (2019 - 1999 + 1)) %>% 
-  ungroup() %>% 
-  mutate(scl_mean_stock = c(scale(mean_stock)))
+source("data_fmt_analysis.R")
 
 
 # plot --------------------------------------------------------------------
 
+## filename
 x <- list.files(path = "data_fmt") %>% 
   str_detect(pattern = "data_ssm")
 
-file_name <- paste0("data_fmt/",
-                    list.files(path = "data_fmt")[x])
 figure_name <- paste0("figure/figure_cv_mu_sd",
                       str_remove(list.files(path = "data_fmt")[x],
                                  "data_ssm_est")) %>% 
   str_replace(".csv", ".pdf")
 
-foreach(i = seq_len(length(file_name))) %do% {
-  
-## ssm estimates ####
-
-df_ssm <- read_csv(file_name[i]) %>% 
-  filter(param_name %in% c("cv", "mu", "sigma")) %>% 
-  rename(median = '50%',
-         high = '97.5%',
-         low = '2.5%') %>% 
-  select(river,
-         site,
-         site_id,
-         param_name,
-         median,
-         high,
-         low)
-
-df_m <- df_ssm %>% 
-  left_join(df_env, by = c("river", "site")) %>% 
-  left_join(df_sp, by = c("river", "site", "site_id")) %>% 
-  left_join(df_stock, by = "river") %>% 
-  left_join(df_ocean, by = "river") %>% 
-  mutate(mean_stock = ifelse(is.na(mean_stock), 0, mean_stock),
-         scl_mean_stock = ifelse(is.na(scl_mean_stock), 0, scl_mean_stock))
-
-
-# plot --------------------------------------------------------------------
-
+## plot
 g1 <- df_m %>% 
   group_by(river, param_name) %>% 
-  summarize(median = mean(median),
+  summarize(value = mean(value),
             mean_stock = unique(mean_stock)) %>% 
-  ggplot() +
-  geom_point(aes(x = mean_stock,
-                 y = median),
-             size = 2) +
-  geom_smooth(aes(x = mean_stock,
-                  y = median),
-              method = "lm",
+  ggplot(aes(x = mean_stock, y = value)) +
+  geom_point(size = 2) +
+  geom_smooth(method = "lm",
               color = grey(0.3)) +
   facet_wrap(facets = ~ param_name,
              ncol = 3,
@@ -95,7 +38,7 @@ g1 <- df_m %>%
   ylab("value") +
   theme_bw()
 
+## export
 ggsave(figure_name[i],
        width = 10,
        height = 3)
-}
