6fec75c5be74f87372650ab31f064d584500506d
aterui <hanabi0111@gmail.com>
2021-10-26 19:36:08 -0400

    Update model_regression_masu.R

--- code_empirical/model_regression_masu.R
+++ code_empirical/model_regression_masu.R
@@ -27,7 +27,7 @@ model {
   ## site-level
   for(i in 1:Nsite) {
     
-    Y[i] ~ dt(mu[i], tau, nu)
+    log_y[i] ~ dt(mu[i], tau, nu)
     
     mu[i] <-
       a0[River[i]] + 
@@ -50,7 +50,7 @@ model {
     
   }
   
-  b_raw[1] <- b[1] - b[2] * mean(Stock[])
+  b_raw[1] <- b[1] - b_raw[2] * mean(Stock[])
   b_raw[2] <- b[2] / sd(Stock[])
   
 }
@@ -60,6 +60,8 @@ data {
   # standardization ---------------------------------------------------------
   
   for(i in 1:Nsite) {
+    log_y[i] <- log(Y[i])
+    
     scl_wsd_area[i] <- (Wsd_area[i] - mean(Wsd_area[])) / sd(Wsd_area[])
     scl_temp[i] <- (Temp[i] - mean(Temp[])) / sd(Temp[])
     scl_ppt[i] <- (Ppt[i] - mean(Ppt[])) / sd(Ppt[])
