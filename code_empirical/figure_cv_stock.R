792e461aaadb28d28d49045e82a227e708cec8a2
aterui <hanabi0111@gmail.com>
2021-10-28 18:47:13 -0400

    Update figure_cv_stock.R

--- code_empirical/figure_cv_stock.R
+++ code_empirical/figure_cv_stock.R
@@ -9,30 +9,42 @@ setwd(here::here("code_empirical"))
 # data --------------------------------------------------------------------
 
 source("data_fmt_analysis.R")
-df_m <- do.call(bind_rows, list_ssm)
+df_m <- do.call(bind_rows, list_ssm) %>%
+  mutate(response = case_when(response == "cv" ~ "CV",
+                              TRUE ~ as.character(response)))
 
 ## raw data
-file_id <- list.files(path = "data_fmt") %>% 
-  str_detect(pattern = "data_ssm")
-
-figure_name <- paste0("figure/figure_cv_mu_sd",
-                      str_remove(list.files(path = "data_fmt")[file_id],
-                                 "data_ssm_est")) %>% 
-  str_replace(".csv", ".pdf")
+figure_name <- paste0("figure/figure_cv_mu_sd_",
+                      c("all", "masu", "other"),
+                      ".pdf")
 
 ## parameter estimates
-file_name <- list.files(path = "result", full.names = TRUE)
+file_name <- list.files(path = "result", full.names = TRUE) %>%
+  as_tibble() %>%
+  filter(str_detect(value, ".csv")) %>%
+  pull()
 
-df_beta <- lapply(file_name, FUN = function(x) {
-    mutate(read_csv(x), group = str_extract(x, "all|masu|other"))
-  }) %>%
+df_beta <- lapply(file_name,
+                  FUN = function(x) {
+                    mutate(read_csv(x),
+                           group = str_extract(x, "all|masu|other"))
+                  }) %>%
   do.call(bind_rows, .) %>%
+  mutate(response = case_when(response == "cv" ~ "CV",
+                              TRUE ~ as.character(response)),
+         pp = prob_positive,
+         pn = 1 - prob_positive) %>%
   filter(str_detect(parameter, "b_raw")) %>%
   pivot_wider(id_cols = c(response, group),
               names_from = parameter,
-              values_from = median) %>%
-  rename(b1 = `b_raw[1]`,
-         b2 = `b_raw[2]`)
+              values_from = c(median, pp, pn)) %>%
+  mutate(prob = ifelse(`pp_b_raw[2]` > `pn_b_raw[2]`,
+                       `pp_b_raw[2]`,
+                       `pn_b_raw[2]`)) %>%
+  rename(b1 = `median_b_raw[1]`,
+         b2 = `median_b_raw[2]`) %>%
+  select(response, group, b1, b2, prob)
+
 
 # plot --------------------------------------------------------------------
 
@@ -48,34 +60,35 @@ df_y <- df_beta %>%
   slice(rep(1:n(), each = 100)) %>% #duplicate for prediction
   group_by(response, group) %>%
   summarize(y = exp(b1 + b2 * x),
-            x = x)
+            x = x,
+            prob = prob)
   
 ## plot
-g <- df_m %>% 
+g_obs <- df_m %>% 
   group_by(river, response, group) %>% 
   summarize(value = mean(value),
             mean_stock = unique(mean_stock)) %>% 
   ggplot(aes(x = mean_stock,
              y = value,
              color = group)) +
-  geom_point(size = 1.5,
-             alpha = 0.3) +
+  geom_point(alpha = 0.3) +
   geom_line(aes(y = y,
-                x = x),
+                x = x,
+                alpha = prob),
+            size = 1,
+            lineend = "round",
+            linejoin = "round",
             data = df_y) +
   facet_wrap(facets = ~ response,
-             ncol = 3,
+             nrow = 3,
              scales = "free_y",
              labeller = label_parsed) + 
-  scale_color_manual(values = c("all" = "black",
-                                "masu" = "salmon",
-                                "other" = "steelblue"),
-                     aesthetics = "color") +
-  xlab("Fish stock (thousand fish)") +
-  ylab("value")# +
-  #scale_y_continuous(trans = "log10")
+  labs(x = "Annual fish stock (thousand fish)",
+       y = "Value",
+       color = "Fish group",
+       alpha = "Prob.")
   
 ## export
 ggsave("figure/figure_cv_mu_sd.pdf",
-       width = 10,
-       height = 3)
+       width = 4,
+       height = 8)
