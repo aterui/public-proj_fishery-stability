4fee2725ad8b84cbc262ec78dfcb7d615d0760be
aterui <hanabi0111@gmail.com>
2021-10-29 18:24:24 -0400

    Update figure_theory_obs.R

--- code_theory/figure_theory_obs.R
+++ code_theory/figure_theory_obs.R
@@ -4,13 +4,13 @@
 rm(list = ls(all.names = TRUE))
 pacman::p_load(tidyverse,
                patchwork)
+
 setwd(here::here())
 
 # data --------------------------------------------------------------------
 
 source("code_theory/figure_theory.R")
 source("code_empirical/figure_cv_stock.R")
-source("code_empirical/figure_coef.R")
 
 g <- g_theory + ggtitle("Theoretical prediction") +
   g_obs + ggtitle("Empirical observation") + 
@@ -18,6 +18,6 @@ g <- g_theory + ggtitle("Theoretical prediction") +
   plot_layout(guides = "collect")
 
 ggsave(g,
-       filename = "document_output/figure02.pdf",
+       filename = "document_output/figure_theory_obs.pdf",
        width = 8,
        height = 9)
