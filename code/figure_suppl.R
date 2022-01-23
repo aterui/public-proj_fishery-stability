9090d4d6cb502b3cec6f524bcd186d95313d445b
AkiraTerui <hanabi0111@gmail.com>
2022-01-23 14:00:49 -0500

    edit: figure_suppl

--- code/figure_suppl.R
+++ code/figure_suppl.R
@@ -1,28 +1,22 @@
 
 # setup -------------------------------------------------------------------
 
-#rm(list = ls(all.names = TRUE))
+rm(list = ls(all.names = TRUE))
 pacman::p_load(tidyverse,
-               patchwork)
+               foreach)
 
 
-# data --------------------------------------------------------------------
+# sensitivity analysis ----------------------------------------------------
 
 ## call `sim_result`
-load(file = "result/result_ricker.RData")
+load(here::here(file = "result/result_ricker.RData"))
 
 df0 <- sim_result %>% 
   mutate(cv = sd_density / mean_density) %>% 
   pivot_longer(cols = mean_density:cv,
                names_to = "response",
                values_to = "value") %>% 
-  filter(r_max == 2,
-         r1 == 1,
-         sd_env == 0.5,
-         alpha == 0.5,
-         phi == 0.8,
-         k == 100,
-         response %in% c("n_sp_persist", "cv", "mean_density", "sd_density")) %>% 
+  filter(response %in% c("n_sp_persist", "cv", "mean_density", "sd_density")) %>% 
   filter(!(response == "cv" & status != "all")) %>%
   filter(!(response == "n_sp_persist" & status != "all")) %>%
   mutate(response_name = case_when(response == "n_sp_persist" ~ "Number~of~species~persist",
@@ -33,46 +27,49 @@ df0 <- sim_result %>%
                               status == "enhanced" ~ "b",
                               status == "unenhanced" ~ "c")) %>% 
   mutate(response_name = factor(response_name,
-                                levels = c("CV~sigma/mu",
-                                           "Number~of~species~persist",
+                                levels = c("Number~of~species~persist",
+                                           "CV~sigma/mu",
                                            "Mean~mu~(ind.)",
                                            "SD~sigma~(ind.)")))
 
+df_param <- df0 %>%
+  distinct(r1, sd_env, k) %>% 
+  arrange(k, r1, sd_env)
 
-# plot1 cv mean sd --------------------------------------------------------
+## plot
 
-source("code/figure_set_theme.R")
+source(here::here("code/figure_set_theme.R"))
 theme_set(plt_theme)
 
-# cv plot ####
-g_theory <- df0 %>% 
-  ggplot(aes(y = value,
-             x = stock,
-             color = group_id,
-             fill = group_id)) +
-  geom_point(data = filter(df0, status == "enhanced"),
-             size = 0.25,
-             color = "darkseagreen2") +
-  geom_point(data = filter(df0, status == "unenhanced"),
-             size = 0.25,
-             color = "lightskyblue2") +
-  geom_point(data = filter(df0, status == "all"),
-             size = 0.25,
-             color = "pink") +
-  geom_smooth(size= 0.1,
-              method = "loess") +
-  scale_color_hue(labels = c("Whole", "Enhanced", "Unenhanced")) +
-  labs(x = "Number of release (individuals)",
-       y = "Value",
-       color = "Species group") +
-  facet_wrap(facets = ~ response_name,
-             nrow = 2,
-             scales = "free_y",
-             labeller = label_parsed) +
-  guides(color = guide_legend(override.aes = list(fill = NA)),
-         fill = "none")
-
-ggsave(g_theory,
-       filename = here::here("figure/figure_theory.pdf"),
-       width = 7.5,
-       height = 6)
+list_g <- foreach(i = seq_len(nrow(df_param))) %do% {
+  
+  ## choose one parameter set
+  df_set <- df0 %>% 
+    filter(r1 == df_param$r1[i],
+           k == df_param$k[i],
+           sd_env == df_param$sd_env[i]) %>% 
+    mutate(alpha_label = case_when(alpha == 0.1 ~ "bar(alpha)==0.1",
+                                   alpha == 0.5 ~ "bar(alpha)==0.5"),
+           phi_label = case_when(phi == 0.8 ~ "f[R]==0.8",
+                                 phi == 1 ~ "f[R]==1.0"))
+  
+  g <- df_set %>% 
+    ggplot(aes(y = value,
+               x = stock,
+               color = group_id,
+               fill = group_id)) +
+    geom_smooth(size= 0.1,
+                method = "loess") +
+    scale_color_hue(labels = c("Whole", "Enhanced", "Unenhanced")) +
+    labs(x = "Number of release (individuals)",
+         color = "Species group") +
+    facet_grid(cols = vars(alpha_label, phi_label),
+               rows = vars(response_name),
+               scales = "free_y",
+               labeller = label_parsed) +
+    guides(color = guide_legend(override.aes = list(fill = NA)),
+           fill = "none") +
+    theme(axis.title.y = element_blank())
+  
+  return(g)
+}
