8330da61a4593e1b5aa1058c0d947e7d98286b6a
AkiraTerui <hanabi0111@gmail.com>
2022-02-02 18:50:36 -0500

    edit: path change

--- code/figure_map.R
+++ code/figure_map.R
@@ -15,19 +15,19 @@ source(here::here("code/gis_crs_fmt.R"))
 source(here::here("code/data_fmt_fishdata.R"))
 
 ## hokkaido shape
-albers_sf_hkd <- st_read(here::here("data_gis/albers_hkd_shape.gpkg"),
+albers_sf_hkd <- st_read(here::here("data_raw/gis/albers_hkd_shape.gpkg"),
                          quiet = TRUE) %>% 
   st_set_crs(wkt_jgd_albers)
 
 ## stream network
-albers_sf_channel <- st_read(here::here("data_gis/albers_channel_hkd.gpkg"),
+albers_sf_channel <- st_read(here::here("data_raw/gis/albers_channel_hkd.gpkg"),
                              quiet = TRUE) %>% 
   st_set_crs(wkt_jgd_albers)
 
 albers_sf_channel_example <- filter(albers_sf_channel, wsd_id == 488) #example
 
 ## watershed
-albers_sf_wsd <- st_read(here::here("data_gis/albers_watershed_hkd_final.gpkg"),
+albers_sf_wsd <- st_read(here::here("data_raw/gis/albers_watershed_hkd_final.gpkg"),
                          quiet = TRUE) %>% 
   st_set_crs(wkt_jgd_albers)
 
@@ -37,7 +37,7 @@ albers_sf_wsd_example <- filter(albers_sf_wsd, wsd_id == 488) #example
 site_selected <- distinct(df_fish, site_id) %>% 
   pull()
 
-albers_sf_point <- st_read(here::here("data_gis/epsg4326_point_snap_prtwsd_edit.gpkg"),
+albers_sf_point <- st_read(here::here("data_raw/gis/epsg4326_point_snap_prtwsd_edit.gpkg"),
                            quiet = TRUE) %>% 
   st_transform(wkt_jgd_albers) %>% 
   mutate(site_id = paste0(river, site)) %>% 
