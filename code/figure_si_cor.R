8d7f6409395552a27dd40d7969fe62a060703738
aterui <hanabi0111@gmail.com>
2022-02-03 18:55:24 -0500

    edit: figure

---
+++ code/figure_si_cor.R
@@ -0,0 +1,34 @@
+
+# setup -------------------------------------------------------------------
+
+rm(list = ls())
+pacman::p_load(tidyverse,
+               corrplot)
+
+
+# data --------------------------------------------------------------------
+
+source(here::here("code/data_fmt_analysis.R"))
+
+
+# plot --------------------------------------------------------------------
+
+M <- df_env %>%
+  left_join(df_stock_mu, by = "river") %>% 
+  left_join(df_ocean, by = "river") %>% 
+  dplyr::select('Forest fraction' = frac_forest,
+                'Agriculture fraction' = frac_agri,
+                'Urban fraction' = frac_urban,
+                'Temperature' = temp,
+                'Precipitation' = ppt,
+                'Watershed area' = wsd_area,
+                'Number of fish released' = mean_stock,
+                'Chlorophyll a' = chr_a) %>% 
+  cor(method = "spearman")
+
+corrplot(M,
+         type="upper",
+         method = "circle",
+         addCoef.col = grey(0.1, alpha = 0.8),
+         diag = FALSE,
+         tl.col = grey(0.1))  
--- code/figure_si_env.R
+++ code/figure_si_env.R
@@ -2,14 +2,21 @@
 # setup -------------------------------------------------------------------
 
 rm(list = ls())
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse,
+               patchwork)
 
 
 # data --------------------------------------------------------------------
 
 source(here::here("code/data_fmt_analysis.R"))
+source(here::here("code/figure_set_theme.R"))
 
-df_env %>% 
+
+# plot --------------------------------------------------------------------
+
+theme_set(plt_theme)
+
+df_site <- df_env %>% 
   pivot_longer(cols = c(wsd_area,
                         temp,
                         ppt,
@@ -22,15 +29,31 @@ df_env %>%
   mutate(var_label = case_when(variable == "frac_agri" ~ "Fraction~agriculture",
                                variable == "frac_forest" ~ "Fraction~forest",
                                variable == "frac_urban" ~ "Fraction~urban",
-                               variable == "ppt" ~ "Cummulative~precipitation~(mm)",
-                               variable == "temp" ~ "Annual~mean~temperature~(degreeC)",
-                               variable == "wsd_area" ~ "Watershed~area~(km^2)"),
-         var_label = factor(var_label, levels = c("Watershed~area~(km^2)",
-                                                  "Annual~mean~temperature~(degreeC)",
-                                                  "Cummulative~precipitation~(mm)",
+                               variable == "ppt" ~ "Precipitation~(mm)",
+                               variable == "temp" ~ "Temperature~(degree*C)",
+                               variable == "wsd_area" ~ "Watershed~area~(km^2)"))
+
+df_river <- df_stock_mu %>% 
+  left_join(df_ocean, by = "river") %>% 
+  pivot_longer(cols = c(mean_stock,
+                        chr_a),
+               names_to = "variable",
+               values_to = "value") %>% 
+  dplyr::select(-c(starts_with("scl"), "sst")) %>% 
+  mutate(var_label = case_when(variable == "mean_stock" ~ "Number~of~releases~(million~fish)",
+                               variable == "chr_a" ~ "Chlorophyll~a~(mg~m^-3)"))
+
+df0 <- bind_rows(df_site, df_river) %>% 
+  mutate(var_label = factor(var_label, levels = c("Watershed~area~(km^2)",
+                                                  "Temperature~(degree*C)",
+                                                  "Precipitation~(mm)",
                                                   "Fraction~forest",
                                                   "Fraction~agriculture",
-                                                  "Fraction~urban"))) %>% 
+                                                  "Fraction~urban",
+                                                  "Number~of~releases~(million~fish)",
+                                                  "Chlorophyll~a~(mg~m^-3)")))
+
+g_env <- df0 %>% 
   ggplot(aes(x = value)) + 
   geom_histogram() +
   facet_wrap(facets = ~var_label,
