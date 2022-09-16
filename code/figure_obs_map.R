2ece0e68ad64283b340f66a74ef0e55fbd500347
aterui <hanabi0111@gmail.com>
2022-09-16 17:40:30 -0400

    Update figure_obs_map.R

--- code/figure_obs_map.R
+++ code/figure_obs_map.R
@@ -1,13 +1,7 @@
 
 # setup -------------------------------------------------------------------
 
-pacman::p_load(tidyverse,
-               patchwork,
-               cowplot,
-               magick,
-               ggspatial,
-               sf)
-
+source(here::here("code/library.R"))
 
 # read data ---------------------------------------------------------------
 
@@ -20,16 +14,22 @@ albers_sf_hkd <- st_read(here::here("data_raw/gis/albers_hkd_shape.gpkg"),
   st_set_crs(wkt_jgd_albers)
 
 ## stream network
-albers_sf_channel <- st_read(here::here("data_raw/gis/albers_channel_hkd.gpkg"),
-                             quiet = TRUE) %>% 
+albers_sf_channel <- readRDS(here::here("data_raw/gis/epsg4326_channel.rds")) %>% 
+  st_transform(wkt_jgd_albers)
+
+## watershed
+albers_sf_wsd <- readRDS(here::here("data_raw/gis/albers_wsd.rds")) %>% 
   st_set_crs(wkt_jgd_albers)
+
+
+# example watershed -------------------------------------------------------
+
+albers_sf_channel_join <- st_join(albers_sf_channel,
+                                  albers_sf_wsd)
 
 albers_sf_channel_example <- filter(albers_sf_channel, wsd_id == 488) #example
 
 ## watershed
-albers_sf_wsd <- st_read(here::here("data_raw/gis/albers_watershed_hkd_final.gpkg"),
-                         quiet = TRUE) %>% 
-  st_set_crs(wkt_jgd_albers)
 
 albers_sf_wsd_example <- filter(albers_sf_wsd, wsd_id == 488) #example
 
