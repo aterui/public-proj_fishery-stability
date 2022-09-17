5ef079ca2ca740156d48d5d572b3faa532cbbf5b
aterui <hanabi0111@gmail.com>
2022-09-17 16:54:28 -0400

    Update run_jags_model_reg.R

--- code/run_jags_model_reg.R
+++ code/run_jags_model_reg.R
@@ -174,7 +174,7 @@ df_est <- as_tibble(mcmc_summary) %>%
 
 # export ------------------------------------------------------------------
 
-saveRDS(df_est, here::here(paste0("output/summary_reg.rds")))
+saveRDS(df_est, here::here("output/summary_reg.rds"))
 saveRDS(mcmc_sample, file = here::here("output/mcmc_reg.rds"))
 MCMCvis::MCMCtrace(mcmc_sample,
                    filename = "output/mcmc_trace_reg")
