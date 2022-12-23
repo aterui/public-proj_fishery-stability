1a64459b93d215f4649d9aaf2143ba4a29b2c1bd
aterui <hanabi0111@gmail.com>
2022-12-23 10:11:41 -0500

    edit: figure map code

--- code/figure_obs_map.R
+++ code/figure_obs_map.R
@@ -13,22 +13,30 @@ albers_sf_hkd <- st_read(here::here("data_raw/gis/albers_hkd_shape.gpkg"),
                          quiet = TRUE) %>% 
   st_set_crs(wkt_jgd_albers)
 
-## stream network
-albers_sf_channel <- readRDS(here::here("data_raw/gis/epsg4326_channel.rds")) %>% 
-  st_transform(wkt_jgd_albers)
-
 ## watershed
 albers_sf_wsd <- readRDS(here::here("data_raw/gis/albers_wsd_outlet.rds")) %>% 
   st_set_crs(wkt_jgd_albers)
 
+## stream network
+albers_sf_channel <- readRDS(here::here("data_raw/gis/epsg4326_channel.rds")) %>% 
+  st_transform(wkt_jgd_albers)
+
+albers_sf_channel_join <- st_join(albers_sf_channel,
+                                  albers_sf_wsd,
+                                  join = st_within) %>%
+  drop_na(river)
+
+## site data
+site_selected <- distinct(df_fish, site_id) %>%
+  pull()
+
+albers_sf_point <- readRDS(here::here("data_raw/gis/epsg4326_point_snap.rds")) %>%
+  st_transform(wkt_jgd_albers) %>%
+  mutate(site_id = paste0(river, site)) %>%
+  filter(site_id %in% site_selected)
 
 # example watershed -------------------------------------------------------
-# 
-# albers_sf_channel_join <- st_join(albers_sf_channel,
-#                                   albers_sf_wsd,
-#                                   join = st_within) %>% 
-#   drop_na(river)
-# 
+
 # ## example watershed (okushibetsu)
 # albers_sf_channel_oku <- filter(albers_sf_channel_join,
 #                                 river == "okushibetsu") #example
@@ -36,15 +44,6 @@ albers_sf_wsd <- readRDS(here::here("data_raw/gis/albers_wsd_outlet.rds")) %>%
 # albers_sf_wsd_oku <- filter(albers_sf_wsd,
 #                             river == "okushibetsu") #example
 # 
-# ## site data
-# site_selected <- distinct(df_fish, site_id) %>% 
-#   pull()
-# 
-# albers_sf_point <- readRDS(here::here("data_raw/gis/epsg4326_point_snap.rds")) %>% 
-#   st_transform(wkt_jgd_albers) %>% 
-#   mutate(site_id = paste0(river, site)) %>% 
-#   filter(site_id %in% site_selected)
-
 
 # figure: map -------------------------------------------------------------
 
