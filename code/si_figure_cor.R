34f9af22a0ed57bd6c1522c414bee2c56956aba6
AkiraTerui <hanabi0111@gmail.com>
2022-12-24 13:14:06 -0500

    Update si_figure_cor.R

--- code/si_figure_cor.R
+++ code/si_figure_cor.R
@@ -14,15 +14,17 @@ source(here::here("code/data_fmt_reg.R"))
 M <- df_env %>%
   left_join(df_stock_mu, by = "river") %>% 
   left_join(df_ocean, by = "river") %>% 
+  left_join(df_n_obs, by = "river") %>% 
   dplyr::select('Forest fraction' = frac_forest,
                 'Agriculture fraction' = frac_agri,
                 'Urban fraction' = frac_urban,
                 'Temperature' = temp,
                 'Precipitation' = ppt,
                 'Watershed area' = wsd_area,
+                'Number of observation years' = n_obs,
                 'Number of fish released' = mean_stock,
                 'Chlorophyll a' = chr_a,
-                'SD Elevation' = sd_elev) %>% 
+                'SD elevation' = sd_elev) %>% 
   cor(method = "spearman")
 
 corrplot(M,
